ef00a1b043c404d58264dd58b82a5cc56e80995d
cvyw <bice881@gmail.com>
2023-12-15 12:21:29 +0800

    弃用small package

--- diy-part2.sh
+++ diy-part2.sh
@@ -43,33 +43,51 @@ sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/UnblockNeteaseMusic-Go package/openwrt-packages
 # 更换netdata为汉化版
 rm -rf ./feeds/luci/applications/luci-app-netdata/
-git clone https://github.com/Jason6111/luci-app-netdata ./feeds/luci/applications/luci-app-netdata/
-# 添加其他软件
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-svn co https://github.com/nantayo/passwall/trunk package/passwall
+git clone https://github.com/Jason6111/luci-app-netdata ./feeds/luci/applications/luci-app-netdata
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+svn co https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+svn co https://github.com/kenzok8/small-package/tree/main/luci-app-shortcutmenu package/luci-app-shortcutmenu
+svn co https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus package/luci-app-wolplus
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+
+# passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git  package/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/brook package/brook
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/chinadns-ng package/chinadns-ng
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/dns2socks package/dns2socks
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/dns2tcp package/dns2tcp
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/gn package/gn
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/hysteria package/hysteria
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/ipt2socks package/ipt2socks
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/microsocks package/microsocks
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/naiveproxy package/naiveproxy
+#svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/pdnsd-alt package/pdnsd-alt #与lean重复feeds/packages/net
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/shadowsocks-rust package/shadowsocks-rust
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/shadowsocksr-libev package/shadowsocksr-libev
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/simple-obfs package/simple-obfs
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/sing-box package/sing-box
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/ssocks package/ssocks
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/tcping package/tcping
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/trojan-go package/trojan-go
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/trojan-plus package/trojan-plus
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/trojan package/trojan
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/tuic-client package/tuic-client
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/v2ray-core package/v2ray-core
+#cp -rf $GITHUB_WORKSPACE/general/v2ray-core package/v2ray-core
+#svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/v2ray-geodata package/v2ray-geodata #与lean重复feeds/packages/net
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/v2ray-plugin package/v2ray-plugin
+#cp -rf $GITHUB_WORKSPACE/general/v2ray-plugin package/v2ray-plugin
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/xray-core package/xray-core
+svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk/xray-plugin package/xray-plugin
 
 # 删除重复包
 
 # rm -rf feeds/luci/applications/luci-app-netdata
 # rm -rf feeds/luci/themes/luci-theme-argon
-# rm -rf package/small-package/luci-app-openvpn-server
-# rm -rf package/small-package/openvpn-easy-rsa-whisky
-# rm -rf package/small-package/luci-app-wrtbwmon
-# rm -rf package/small-package/wrtbwmon
-# rm -rf package/small-package/luci-app-koolproxyR
-# rm -rf package/small-package/luci-app-godproxy
-# rm -rf package/small-package/luci-app-argon*
-# rm -rf package/small-package/luci-theme-argon*
-# rm -rf package/small-package/luci-app-amlogic
-# rm -rf package/small-package/luci-app-unblockneteasemusic
-# rm -rf package/small-package/opkg
 # 
 # # 编译问题
 # rm -rf package/small-package/upx
-# rm -rf package/small-package/upx-static
-# rm -rf package/small-package/firewall*
-rm -rf package/small-package/miniupnpd-iptables
-rm -rf feeds/small8/miniupnpd-iptables
 
 # 其他调整
 # NAME=$"package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
@@ -81,9 +99,18 @@ rm -rf feeds/small8/miniupnpd-iptables
 # curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
 # curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key
 
-sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/quanjindeng/Actions_OpenWrt-Amlogic#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
-sed -i 's#ARMv8#openwrt_armvirt_v8#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
-sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+# Amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# Modify the default configuration of Amlogic Box
+# 1.Set the download repository of the OpenWrt files to your github.com（OpenWrt 文件的下载仓库）
+sed -i "s|https.*/OpenWrt|https://github.com/Futurescope/Actions_OpenWrt-Amlogic|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 2.Set the keywords of Tags in your github.com Releases（Releases 里 Tags 的关键字）
+sed -i "s|ARMv8|openwrt_N1|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 3.Set the suffix of the OPENWRT files in your github.com Releases（Releases 里 OpenWrt 文件的后缀）
+#sed -i "s|.img.gz|_Full.img.gz|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 4.Set the download path of the kernel in your github.com repository（OpenWrt 内核的下载路径）
+sed -i "s|opt/kernel|https://github.com/breakings/OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
+
 
 sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-mount/root/etc/init.d/cifs
 
